ad3b21785dbb4b8fd78062422e36366c7ec31a94
Vicente <vicho0202@gmail.com>
2022-08-23 12:53:48 -0400

    Added tabs, custom icon, etc.

--- YmapPropSplitter/Form1.Designer.cs
+++ YmapPropSplitter/Form1.Designer.cs
@@ -29,91 +29,57 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.tbYTYP = new System.Windows.Forms.TextBox();
-            this.tbYMAP = new System.Windows.Forms.TextBox();
-            this.btnBrowseYTYP = new System.Windows.Forms.Button();
-            this.btnBrowseYMAP = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.tbOutput = new System.Windows.Forms.TextBox();
-            this.btnBrowseOutput = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btnSplit = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.lbYTYPstatus = new System.Windows.Forms.Label();
-            this.lbYmap = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lbYmap = new System.Windows.Forms.Label();
+            this.lbYTYPstatus = new System.Windows.Forms.Label();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.btnBrowseYMAP = new System.Windows.Forms.Button();
+            this.btnBrowseYTYP = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.tbYMAP = new System.Windows.Forms.TextBox();
+            this.tbYTYP = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tbYTYP
+            // tabControl1
             // 
-            resources.ApplyResources(this.tbYTYP, "tbYTYP");
-            this.tbYTYP.Name = "tbYTYP";
-            this.tbYTYP.TextChanged += new System.EventHandler(this.tbYTYP_TextChanged);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
             // 
-            // tbYMAP
+            // tabPage1
             // 
-            resources.ApplyResources(this.tbYMAP, "tbYMAP");
-            this.tbYMAP.Name = "tbYMAP";
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.lbYmap);
+            this.tabPage1.Controls.Add(this.lbYTYPstatus);
+            this.tabPage1.Controls.Add(this.btnSplit);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnBrowseOutput);
+            this.tabPage1.Controls.Add(this.btnBrowseYMAP);
+            this.tabPage1.Controls.Add(this.btnBrowseYTYP);
+            this.tabPage1.Controls.Add(this.tbOutput);
+            this.tabPage1.Controls.Add(this.tbYMAP);
+            this.tabPage1.Controls.Add(this.tbYTYP);
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // btnBrowseYTYP
+            // progressBar1
             // 
-            resources.ApplyResources(this.btnBrowseYTYP, "btnBrowseYTYP");
-            this.btnBrowseYTYP.Name = "btnBrowseYTYP";
-            this.btnBrowseYTYP.UseVisualStyleBackColor = true;
-            this.btnBrowseYTYP.Click += new System.EventHandler(this.btnBrowseYTYP_Click);
-            // 
-            // btnBrowseYMAP
-            // 
-            resources.ApplyResources(this.btnBrowseYMAP, "btnBrowseYMAP");
-            this.btnBrowseYMAP.Name = "btnBrowseYMAP";
-            this.btnBrowseYMAP.UseVisualStyleBackColor = true;
-            this.btnBrowseYMAP.Click += new System.EventHandler(this.btnBrowseYMAP_Click);
-            // 
-            // label1
-            // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
-            // 
-            // label2
-            // 
-            resources.ApplyResources(this.label2, "label2");
-            this.label2.Name = "label2";
-            // 
-            // tbOutput
-            // 
-            resources.ApplyResources(this.tbOutput, "tbOutput");
-            this.tbOutput.Name = "tbOutput";
-            // 
-            // btnBrowseOutput
-            // 
-            resources.ApplyResources(this.btnBrowseOutput, "btnBrowseOutput");
-            this.btnBrowseOutput.Name = "btnBrowseOutput";
-            this.btnBrowseOutput.UseVisualStyleBackColor = true;
-            this.btnBrowseOutput.Click += new System.EventHandler(this.btnBrowseOutput_Click);
-            // 
-            // label3
-            // 
-            resources.ApplyResources(this.label3, "label3");
-            this.label3.Name = "label3";
-            // 
-            // btnSplit
-            // 
-            resources.ApplyResources(this.btnSplit, "btnSplit");
-            this.btnSplit.Name = "btnSplit";
-            this.btnSplit.UseVisualStyleBackColor = true;
-            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
-            // 
-            // label4
-            // 
-            resources.ApplyResources(this.label4, "label4");
-            this.label4.Name = "label4";
-            // 
-            // lbYTYPstatus
-            // 
-            resources.ApplyResources(this.lbYTYPstatus, "lbYTYPstatus");
-            this.lbYTYPstatus.ForeColor = System.Drawing.Color.IndianRed;
-            this.lbYTYPstatus.Name = "lbYTYPstatus";
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
             // 
             // lbYmap
             // 
@@ -121,51 +87,103 @@
             this.lbYmap.ForeColor = System.Drawing.Color.IndianRed;
             this.lbYmap.Name = "lbYmap";
             // 
-            // progressBar1
+            // lbYTYPstatus
             // 
-            resources.ApplyResources(this.progressBar1, "progressBar1");
-            this.progressBar1.Name = "progressBar1";
+            resources.ApplyResources(this.lbYTYPstatus, "lbYTYPstatus");
+            this.lbYTYPstatus.ForeColor = System.Drawing.Color.IndianRed;
+            this.lbYTYPstatus.Name = "lbYTYPstatus";
+            // 
+            // btnSplit
+            // 
+            resources.ApplyResources(this.btnSplit, "btnSplit");
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // btnBrowseOutput
+            // 
+            resources.ApplyResources(this.btnBrowseOutput, "btnBrowseOutput");
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowseYMAP
+            // 
+            resources.ApplyResources(this.btnBrowseYMAP, "btnBrowseYMAP");
+            this.btnBrowseYMAP.Name = "btnBrowseYMAP";
+            this.btnBrowseYMAP.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowseYTYP
+            // 
+            resources.ApplyResources(this.btnBrowseYTYP, "btnBrowseYTYP");
+            this.btnBrowseYTYP.Name = "btnBrowseYTYP";
+            this.btnBrowseYTYP.UseVisualStyleBackColor = true;
+            // 
+            // tbOutput
+            // 
+            resources.ApplyResources(this.tbOutput, "tbOutput");
+            this.tbOutput.Name = "tbOutput";
+            // 
+            // tbYMAP
+            // 
+            resources.ApplyResources(this.tbYMAP, "tbYMAP");
+            this.tbYMAP.Name = "tbYMAP";
+            // 
+            // tbYTYP
+            // 
+            resources.ApplyResources(this.tbYTYP, "tbYTYP");
+            this.tbYTYP.Name = "tbYTYP";
+            // 
+            // tabPage2
+            // 
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.lbYmap);
-            this.Controls.Add(this.lbYTYPstatus);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.btnSplit);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnBrowseOutput);
-            this.Controls.Add(this.btnBrowseYMAP);
-            this.Controls.Add(this.btnBrowseYTYP);
-            this.Controls.Add(this.tbOutput);
-            this.Controls.Add(this.tbYMAP);
-            this.Controls.Add(this.tbYTYP);
+            this.Controls.Add(this.tabControl1);
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private TextBox tbYTYP;
-        private TextBox tbYMAP;
-        private Button btnBrowseYTYP;
-        private Button btnBrowseYMAP;
-        private Label label1;
-        private Label label2;
-        private TextBox tbOutput;
-        private Button btnBrowseOutput;
-        private Label label3;
-        private Button btnSplit;
-        private Label label4;
-        private Label lbYTYPstatus;
-        private Label lbYmap;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
         private ProgressBar progressBar1;
+        private Label lbYmap;
+        private Label lbYTYPstatus;
+        private Button btnSplit;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button btnBrowseOutput;
+        private Button btnBrowseYMAP;
+        private Button btnBrowseYTYP;
+        private TextBox tbOutput;
+        private TextBox tbYMAP;
+        private TextBox tbYTYP;
+        private TabPage tabPage2;
     }
 }
