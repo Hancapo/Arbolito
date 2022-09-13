843363e8ec32c71663f488cdbe1105c8d35bc217
Vicente <vicho0202@gmail.com>
2022-09-12 21:20:41 -0500

    Changed project name to Arbolito

--- YmapPropSplitter/Properties/Resources.Designer.cs
+++ YmapPropSplitter/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace YmapPropSplitter.Properties {
+namespace Arbolito.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace YmapPropSplitter.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("YmapPropSplitter.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Arbolito.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
